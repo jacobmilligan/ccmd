6802bb0cad50cbe925ec70e84843787e2fee38b4
Jacob Milligan <jacobpmilligan@gmail.com>
2022-01-27 17:21:22 +1100

    rename project to ccmd (no longer associated with bee). Fix run callbacks being discarded by command results

--- example/basic.c
+++ example/basic.c
@@ -1,11 +1,11 @@
 /*
  *  cmd.c
- *  cli
+ *  ccmd
  *
  *  Copyright (c) 2021 Jacob Milligan. All rights reserved.
  */
 
-#include <cli/cli.h>
+#include <ccmd.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,16 +13,16 @@
 #define FILE_BUFFER_SIZE 4096
 static char file_buffer[FILE_BUFFER_SIZE] = { 0 };
 
-cli_status print_header_command(const cli_command_result* program, const cli_command_result* command)
+ccmd_status print_header_command(const ccmd_command_result* program, const ccmd_command_result* command)
 {
-    const bool verbose = cli_has_option(program, "verbose");
+    const bool verbose = ccmd_has_option(program, "verbose");
 
     if (verbose)
     {
         printf("%s [verbose]: printing header\n", program->name);
     }
 
-    if (cli_has_option(command, "u"))
+    if (ccmd_has_option(command, "u"))
     {
         printf("--- READ FILE AND PRINT V1.0 ---\n\n");
     }
@@ -31,72 +31,73 @@ cli_status print_header_command(const cli_command_result* program, const cli_com
         printf("--- Read file and print v1.0 ---\n\n");
     }
 
-    return CLI_STATUS_SUCCESS;
+    return CCMD_STATUS_SUCCESS;
 }
 
 int main(int argc, char** argv)
 {
-    cli_result result;
-    cli_status status = cli_parse(&result, argc, argv, &(cli_command) {
+    ccmd_command_result commands[8];
+    ccmd_parsed_args options[16];
+    ccmd_result result = { .commands = CCMD_ARRAY_VIEW(commands), .options = CCMD_ARRAY_VIEW(options) };
+    ccmd_status status = ccmd_parse(&result, argc, argv, &(ccmd_command) {
         .help = "Reads some files and prints them to stdout",
-        .positionals = CLI_ARRAY_VIEW((cli_positional[]) {
+        .positionals = CCMD_ARRAY_VIEW((ccmd_positional[]) {
             { .name = "first-file", .help = "File to read and print to stdout" }
         }),
-        .options = CLI_ARRAY_VIEW((cli_option[]) {
+        .options = CCMD_ARRAY_VIEW((ccmd_option[]) {
             { .short_name = 'v', .long_name = "verbose", .help = "Prints status of the commands", .nargs = 0, .required = false },
             { .short_name = 's', .long_name = "second-file", .help = "Path to another file to read and append to stdout", .nargs = 1, .required = true },
         }),
-        .subcommands = CLI_ARRAY_VIEW((cli_command[]) {
+        .subcommands = CCMD_ARRAY_VIEW((ccmd_command[]) {
             {
                 .name = "print-header",
                 .help = "Prints a header before printing any file contents",
                 .run = print_header_command, // callback executed if this subcommand is requested
-                .options = CLI_ARRAY_VIEW((cli_option[]){
+                .options = CCMD_ARRAY_VIEW((ccmd_option[]){
                     { .short_name = 'u', .long_name = "--uppercase", .help = "Print an UPPERCASE header", .nargs = 0, .required = false },
                 })
             }
         })
     });
 
-    if (status != CLI_STATUS_SUCCESS)
+    if (status != CCMD_STATUS_SUCCESS)
     {
-        return status == CLI_STATUS_ERROR ? EXIT_FAILURE : EXIT_SUCCESS;
+        return status == CCMD_STATUS_ERROR ? EXIT_FAILURE : EXIT_SUCCESS;
     }
 
-    const cli_command_result* program = &result.commands[0];
-    const bool verbose = cli_has_option(program, "verbose");
+    const bool verbose = ccmd_has_option(result.program_command, "verbose");
 
     if (verbose)
     {
-        printf("%s [verbose]: Verbose output is turned on\n", program->name);
+        printf("%s [verbose]: Verbose output is turned on\n", result.program_command->name);
     }
 
     // run any subcommands that might have been encountered before printing the files
-    if (cli_run(&result) != CLI_STATUS_SUCCESS)
+    if (ccmd_run(&result) != CCMD_STATUS_SUCCESS)
     {
         return EXIT_FAILURE;
     }
 
     const char* paths[] = {
-        cli_get_positional(program, 0),                     // first file
-        cli_get_option(program, "second-file")->args[0],    // second file
+        ccmd_get_positional(result.program_command, 0),                     // first file
+        ccmd_get_option(result.program_command, "second-file")->args[0],    // second file
     };
 
     if (verbose)
     {
-        for (int i = 0; i < CLI_ARRAY_SIZE(paths); ++i)
+        for (int i = 0; i < CCMD_ARRAY_SIZE(paths); ++i)
         {
-            printf("%s [verbose]: file %d: %s\n", program->name, i, paths[0]);
+            printf("%s [verbose]: file %d: %s\n", result.program_command->name, i, paths[0]);
         }
     }
 
-    for (int i = 0; i < CLI_ARRAY_SIZE(paths); ++i)
+    for (int i = 0; i < CCMD_ARRAY_SIZE(paths); ++i)
     {
         FILE* file = fopen(paths[i], "r");
         {
             if (file == NULL)
             {
-                fprintf(stderr, "%s: error: invalid file path: %s\n", program->name, paths[i]);
+                fprintf(stderr, "%s: error: invalid file path: %s\n", result.program_command->name, paths[i]);
                 return EXIT_FAILURE;
             }
 
